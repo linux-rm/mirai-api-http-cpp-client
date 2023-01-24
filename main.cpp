77ca75b13c7c3010ab7f8ec94b55a7c0e112a54d
linux-rm <linux-rm@outlook.com>
2023-01-25 01:01:45 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,44 @@
+/*************************************************************************
+ > Author: linux-rm
+ > Mail: linux-rm@outlook.com
+ > QQ:1158290244
+ > QQ IT group:951234938
+ ************************************************************************/
+
+#include <iostream>
+#include "msgbox.hpp"
+#include "httpC.hpp"
+
+using namespace std;
+
+void breakc(int sig)
+{
+	if (sig == SIGINT)
+	{
+		std::cout
+			<< REAST << std::endl
+			<< "\e]0;shell!\a" << flush; // 恢复终端和标题
+		exit(0);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	signal(SIGINT, breakc); // SIGINT信号处理
+	FLUSH_SIZE;
+	if (argc == 3 || argc == 4)
+	{
+		msgbox("欢迎使用mirai-api-http-cpp", "欢迎使用mirai-api-http-cpp");
+		if (argc == 3)
+			httpC(argv[1], argv[2]);
+		else if (argc == 4)
+			httpC(argv[1], argv[2], argv[3]);
+		return 0;
+	}
+	else
+	{
+		msgbox("帮助", "用法: ./mcc <路径> <请求方式 = get> [请求正文(POST)='\\0']\n示例: ./mcc /about get", "知道了");
+		return 1;
+	}
+	return -1;
+}
